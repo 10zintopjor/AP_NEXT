10e62cbe8b0e7373c0e81e6207794564f2ebb2b7
jungtop <jungtop@jungtops-MacBook-Pro.local>
2024-08-24 11:19:26 -0400

    updated app

--- src/app/components/Mobilenav.js
+++ src/app/components/Mobilenav.js
@@ -23,7 +23,7 @@ const Mobilenav = () => {
             {/* Logo */}
             <div className="p-1"> {/* Added md:pl-4 for desktop padding */}
             <Link href="/">
-            <Image src='/images/8.png' alt="logo" width={100} height={60} className="h-24 p-3 md:cursor-pointer" />
+            <Image src='/images/8.png' alt="logo" width={300} height={60} className="h-24 p-3 md:cursor-pointer" />
             
             </Link>
             </div>
